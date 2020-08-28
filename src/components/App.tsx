e50caebedba78a596def4a738447d834c19489fe
Lubomir <lubomir.yudenko@gmail.com>
2020-08-28 14:24:02 +0300

    feat(*): add basics
    Add all basic config files to work with typescipt and react

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default class App extends React.Component {
+	render(): JSX.Element {
+		return (
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+				}}
+			>
+				<h1>Cookie clicker rewritten!</h1>
+			</div>
+		)
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path")
+const HTMLPlugin = require("html-webpack-plugin")
+//const ArchivePlugin = require("webpack-archive-plugin")
+//const { name } = require("./package.json")
+let plugins = [
+	new HTMLPlugin({
+		template: "./src/index.html",
+		filename: "index.html",
+	}),
+]
+/*if (process.env.NODE_ENV === "production")
+	plugins.push(new ArchivePlugin({ format: "tar", output: `./dist/${name}` }))*/
+module.exports = {
+	entry: "./src/index.tsx",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "main.js",
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: ["ts-loader"],
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+				loader: "file-loader",
+			},
+		],
+	},
+	mode: process.env.NODE_ENV,
+	resolve: { extensions: [".tsx", ".ts", ".jsx", ".js", ".json"] },
+	plugins,
+	watchOptions: {
+		ignored: ["node_modules"],
+	},
+}
