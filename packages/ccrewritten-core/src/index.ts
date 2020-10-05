c147dc019184a5f587a934b107cfccea28f936ad
Lubomir <lubomir.yudenko@gmail.com>
2020-10-05 22:20:16 +0300

    Add ccrewritten-core

---
+++ packages/ccrewritten-core/src/index.ts
@@ -0,0 +1,3 @@
+console.log("CCRewritten core activated!")
+
+export default "Hello!"
---
+++ packages/ccrewritten-core/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path")
+
+module.exports = {
+	entry: "./src/index.ts",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "main.js",
+	},
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: ["ts-loader"],
+			},
+		],
+	},
+	mode: process.env.NODE_ENV,
+	resolve: { extensions: [".tsx", ".ts", ".jsx", ".js", ".json"] },
+	plugins: [],
+	watchOptions: {
+		ignored: ["node_modules"],
+	},
+}
